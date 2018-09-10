72fedcd2d0950db812bd7f6ec9f3c9ff2d9f4e20
Akiyoshi <akiyoshi.git@gmail.com>
2018-09-10 14:10:51 +0000

    add column

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,10 +4,21 @@
 
     <h1>タスク新規作成</h1>
     
+    @if (count($errors) > 0)
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
+    
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
         
         {!! Form::label('content', 'タスク：') !!}
         {!! Form::text('content') !!}
+        
+        {!! Form::label('status', 'ステータス：') !!}
+        {!! Form::text('status') !!}
         
         {!! Form::submit('投稿！') !!}
         
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -4,6 +4,7 @@
 
     <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
     
+    <p>{{ $task->status }}</p>
     <p>{{ $task->content }}</p>
     
     {!! link_to_route('tasks.edit', 'このメッセージを編集', ['id' => $task->id]) !!}
