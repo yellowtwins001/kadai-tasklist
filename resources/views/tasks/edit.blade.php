6282479080ff4dbe89661b35c937778941e280d7
Akiyoshi <akiyoshi.git@gmail.com>
2018-09-15 07:27:33 +0000

    add bootstrap design col-sm

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     <div class="row">
-        <div class="col-xs-12 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
             <h1>id = {{ $task->id }}のタスク編集ページ</h1>
             
             {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
